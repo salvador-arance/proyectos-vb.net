d1dc994d57bb602468ef636a881d39cabb9729ae
salva <salvaarancegallego@gmail.com>
2025-12-10 10:32:25 +0100

    Avanzar algo más en clase

--- Ejercicios/ejercicio8/ejercicio_8_4/ejercicio_8_4/Module1.vb
+++ Ejercicios/ejercicio8/ejercicio_8_4/ejercicio_8_4/Module1.vb
@@ -2,35 +2,37 @@
     Dim personas() As Persona = {}
     Dim mascotas() As Mascota = {}
     Sub Main()
-        Dim persona As New Persona
-        Dim mascota As New Mascota
+        Dim persona As Persona
+        Dim mascota As Mascota
         Dim entrada As String
         Dim tieneNoLetras As Boolean
 
         Do
             Console.WriteLine("DATOS PERSONA")
+            persona = New Persona
             Do
                 Console.Write("Nombre: ")
                 entrada = Console.ReadLine
-
-                For i = 0 To entrada.Length - 1
-                    tieneNoLetras = Not (Char.IsLetter(entrada.ElementAt(i)) OrElse Char.IsWhiteSpace(entrada.ElementAt(i)))
-                    If tieneNoLetras Then
-                        Exit For
-                    End If
-                Next
 
                 If String.IsNullOrWhiteSpace(entrada) Then
                     Console.ForegroundColor = ConsoleColor.Red
                     Console.WriteLine("El nombre no puede quedarse en blanco.")
                     Console.ResetColor()
+                Else
+                    For i = 0 To entrada.Length - 1
+                        tieneNoLetras = Not (Char.IsLetter(entrada.ElementAt(i)) OrElse Char.IsWhiteSpace(entrada.ElementAt(i)))
+                        If tieneNoLetras Then
+                            Exit For
+                        End If
+                    Next
+                    If tieneNoLetras Then
+                        Console.ForegroundColor = ConsoleColor.Red
+                        Console.WriteLine("El nombre solo puede tener letras o espacios en blanco.")
+                        Console.ResetColor()
+                    End If
                 End If
 
-                If Not String.IsNullOrWhiteSpace(entrada) AndAlso tieneNoLetras Then
-                    Console.ForegroundColor = ConsoleColor.Red
-                    Console.WriteLine("El nombre solo puede tener letras o espacios en blanco.")
-                    Console.ResetColor()
-                End If
+
             Loop While String.IsNullOrWhiteSpace(entrada) OrElse tieneNoLetras
 
             persona.Nombre = entrada
@@ -44,8 +46,19 @@
                     Console.ResetColor()
                 End If
             Loop While Not Date.TryParse(entrada, persona.FechaNacimiento) OrElse persona.FechaNacimiento > Today
-            Console.WriteLine("Conseguido")
-
+            Do
+                Console.WriteLine("¿Tiene mascota? (S/N)")
+                entrada = Console.ReadLine.ToUpper
+                If entrada <> "S" AndAlso entrada <> "N" Then
+                    Console.ForegroundColor = ConsoleColor.Red
+                    Console.WriteLine("Introduce S o N.")
+                    Console.ResetColor()
+                End If
+            Loop While entrada <> "S" AndAlso entrada <> "N"
+            'TODO Terminar esto hoy en casa más tranquilo.
+            If entrada = "S" Then
+                mascota = New Mascota
+            End If
         Loop While entrada.equals("S")
     End Sub
 
