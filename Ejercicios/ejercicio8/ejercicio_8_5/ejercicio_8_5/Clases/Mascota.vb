587b2ba43a5592790f1e38294070e66ffa1eabd1
salva <salvaarancegallego@gmail.com>
2025-12-11 12:42:13 +0100

    Añadir 8_5

---
+++ Ejercicios/ejercicio8/ejercicio_8_5/ejercicio_8_5/Clases/Mascota.vb
@@ -0,0 +1,51 @@
+﻿Public Class Mascota
+    Private _Nombre As String
+    Private _FechaNacimiento As Date
+    Private _Raza As String
+    Private _TipoMascota As String
+    Private _Dueño As Persona
+    Public Property Nombre As String
+        Get
+            Return _Nombre
+        End Get
+        Set(value As String)
+            _Nombre = value
+        End Set
+    End Property
+
+    Public Property FechaNacimiento As Date
+        Get
+            Return _FechaNacimiento
+        End Get
+        Set(value As Date)
+            _FechaNacimiento = value
+        End Set
+    End Property
+
+    Public Property Raza As String
+        Get
+            Return _Raza
+        End Get
+        Set(value As String)
+            _Raza = value
+        End Set
+    End Property
+
+    Public Property TipoMascota As String
+        Get
+            Return _TipoMascota
+        End Get
+        Set(value As String)
+            _TipoMascota = value
+        End Set
+    End Property
+
+    Public Property Dueño As Persona
+        Get
+            Return _Dueño
+        End Get
+        Set(value As Persona)
+            _Dueño = value
+        End Set
+    End Property
+End Class
---
+++ Ejercicios/ejercicio8/ejercicio_8_5/ejercicio_8_5/Clases/Persona.vb
@@ -0,0 +1,31 @@
+﻿Public Class Persona
+    Private _Nombre As String
+    Private _FechaNacimiento As Date
+    Private _SuMascota As Mascota
+    Public Property Nombre As String
+        Get
+            Return _Nombre
+        End Get
+        Set(value As String)
+            _Nombre = value
+        End Set
+    End Property
+
+    Public Property FechaNacimiento As Date
+        Get
+            Return _FechaNacimiento
+        End Get
+        Set(value As Date)
+            _FechaNacimiento = value
+        End Set
+    End Property
+
+    Public Property SuMascota As Mascota
+        Get
+            Return _SuMascota
+        End Get
+        Set(value As Mascota)
+            _SuMascota = value
+        End Set
+    End Property
+End Class
