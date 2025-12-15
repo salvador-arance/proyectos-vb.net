09a00d4575d18c900d6296c719e729a0c4eb2b88
salvador-arance <salvaarancegallego@gmail.com>
2025-12-15 16:29:02 +0100

    Copiar 8_4 en 8_5

--- Ejercicios/ejercicio8/ejercicio_8_5/ejercicio_8_5/Module1.vb
+++ Ejercicios/ejercicio8/ejercicio_8_5/ejercicio_8_5/Module1.vb
@@ -1,212 +1,132 @@
 ﻿Module Module1
-    Dim personas() As Persona = {}
+    Dim persona As Persona
+    Dim mascota As Mascota
+    Dim _perMasJoven As Persona
+    Dim _mascotaMasVieja As Mascota
 
-    'TODO Terminar 8_4 y copiarlo aquí para luego seguir.
     Sub Main()
-
-        Dim persona As Persona
-        Dim perMasJoven As Persona
-        Dim mascota As Mascota
-        Dim mascotaMasVieja As Mascota
         Dim entrada As String
-        Dim tieneNoLetras As Boolean
-
         Do
-            Console.WriteLine("DATOS PERSONA")
+            MostrarMensaje($"DATOS PERSONA {vbCrLf}", ConsoleColor.Blue)
             persona = New Persona
+
+            persona.Nombre = PedirTexto("Nombre: ")
+
             Do
-                Console.Write("Nombre: ")
+                MostrarMensaje("Fecha de nacimiento: ", ConsoleColor.White)
                 entrada = Console.ReadLine
+            Loop While FechaNacimientoValida(entrada) = False
+            Date.TryParse(entrada, persona.FechaNacimiento)
 
-                If String.IsNullOrWhiteSpace(entrada) Then
-                    Console.ForegroundColor = ConsoleColor.Red
-                    Console.WriteLine("El nombre no puede quedarse en blanco.")
-                    Console.ResetColor()
-                Else
-                    For i = 0 To entrada.Length - 1
-                        tieneNoLetras = Not (Char.IsLetter(entrada.ElementAt(i)) OrElse Char.IsWhiteSpace(entrada.ElementAt(i)))
-                        If tieneNoLetras Then
-                            Exit For
-                        End If
-                    Next
-                    If tieneNoLetras Then
-                        Console.ForegroundColor = ConsoleColor.Red
-                        Console.WriteLine("El nombre solo puede tener letras o espacios en blanco.")
-                        Console.ResetColor()
-                    End If
-                End If
-
-
-            Loop While String.IsNullOrWhiteSpace(entrada) OrElse tieneNoLetras
-
-            persona.Nombre = entrada
+            ActualizarPersonaMasJoven(persona)
 
             Do
-                Console.Write("Fecha de nacimiento: ")
-                entrada = Console.ReadLine
-                If Not Date.TryParse(entrada, persona.FechaNacimiento) OrElse persona.FechaNacimiento > Today Then
-                    Console.ForegroundColor = ConsoleColor.Red
-                    Console.WriteLine("Debes introducir una fecha anterior a la de hoy.")
-                    Console.ResetColor()
+                MostrarMensaje("¿Tiene mascota (S/N)?: ", ConsoleColor.White)
+                entrada = Console.ReadLine().ToUpper
+                If Not SioNo(entrada) Then
+                    MostrarMensaje("Debe introducir S / N", ConsoleColor.Red)
                 End If
-            Loop While Not Date.TryParse(entrada, persona.FechaNacimiento) OrElse persona.FechaNacimiento > Today
-
-            If IsNothing(perMasJoven) Then
-                perMasJoven = persona
-            Else
-                If persona.FechaNacimiento > perMasJoven.FechaNacimiento Then
-                    perMasJoven = persona
-                End If
-            End If
-
-            Do
-                Console.Write("¿Tiene mascota? (S/N): ")
-                entrada = Console.ReadLine.ToUpper
-                If entrada <> "S" AndAlso entrada <> "N" Then
-                    Console.ForegroundColor = ConsoleColor.Red
-                    Console.WriteLine("Introduce S o N.")
-                    Console.ResetColor()
-                End If
-            Loop While entrada <> "S" AndAlso entrada <> "N"
+            Loop While SioNo(entrada) = False
 
             If entrada = "S" Then
                 mascota = New Mascota
-                mascota.Dueño = persona
-                Console.WriteLine("DATOS DE SU MASCOTA")
+                MostrarMensaje($"DATOS DE SU MASCOTA {vbCrLf}", ConsoleColor.Blue)
+
+                mascota.Nombre = PedirTexto("Nombre: ")
+
+                mascota.TipoMascota = PedirTexto("Tipo: ")
+
+                mascota.Raza = PedirTexto("Raza: ")
 
                 Do
-                    Console.Write("Nombre: ")
-                    entrada = Console.ReadLine
+                    MostrarMensaje("Fecha de nacimiento: ", ConsoleColor.White)
+                    entrada = Console.ReadLine()
+                Loop While FechaNacimientoValida(entrada) = False
+                Date.TryParse(entrada, mascota.FechaNacimiento)
 
-                    If String.IsNullOrWhiteSpace(entrada) Then
-                        Console.ForegroundColor = ConsoleColor.Red
-                        Console.WriteLine("El nombre no puede quedarse en blanco.")
-                        Console.ResetColor()
-                    Else
-                        For i = 0 To entrada.Length - 1
-                            tieneNoLetras = Not (Char.IsLetter(entrada.ElementAt(i)) OrElse Char.IsWhiteSpace(entrada.ElementAt(i)))
-                            If tieneNoLetras Then
-                                Exit For
-                            End If
-                        Next
-                        If tieneNoLetras Then
-                            Console.ForegroundColor = ConsoleColor.Red
-                            Console.WriteLine("El nombre solo puede tener letras o espacios en blanco.")
-                            Console.ResetColor()
-                        End If
-                    End If
-                Loop While String.IsNullOrWhiteSpace(entrada) OrElse tieneNoLetras
-                mascota.Nombre = entrada
-
-                Do
-                    Console.Write("Tipo: ")
-                    entrada = Console.ReadLine
-
-                    If String.IsNullOrWhiteSpace(entrada) Then
-                        Console.ForegroundColor = ConsoleColor.Red
-                        Console.WriteLine("El tipo no puede quedarse en blanco.")
-                        Console.ResetColor()
-                    Else
-                        For i = 0 To entrada.Length - 1
-                            tieneNoLetras = Not (Char.IsLetter(entrada.ElementAt(i)) OrElse Char.IsWhiteSpace(entrada.ElementAt(i)))
-                            If tieneNoLetras Then
-                                Exit For
-                            End If
-                        Next
-                        If tieneNoLetras Then
-                            Console.ForegroundColor = ConsoleColor.Red
-                            Console.WriteLine("El tipo solo puede tener letras o espacios en blanco.")
-                            Console.ResetColor()
-                        End If
-                    End If
-                Loop While String.IsNullOrWhiteSpace(entrada) OrElse tieneNoLetras
-                mascota.TipoMascota = entrada
-
-                Do
-                    Console.Write("Raza: ")
-                    entrada = Console.ReadLine
-
-                    If String.IsNullOrWhiteSpace(entrada) Then
-                        Console.ForegroundColor = ConsoleColor.Red
-                        Console.WriteLine("La raza no puede quedarse en blanco.")
-                        Console.ResetColor()
-                    Else
-                        For i = 0 To entrada.Length - 1
-                            tieneNoLetras = Not (Char.IsLetter(entrada.ElementAt(i)) OrElse Char.IsWhiteSpace(entrada.ElementAt(i)))
-                            If tieneNoLetras Then
-                                Exit For
-                            End If
-                        Next
-                        If tieneNoLetras Then
-                            Console.ForegroundColor = ConsoleColor.Red
-                            Console.WriteLine("La raza solo puede tener letras o espacios en blanco.")
-                            Console.ResetColor()
-                        End If
-                    End If
-                Loop While String.IsNullOrWhiteSpace(entrada) OrElse tieneNoLetras
-                mascota.Raza = entrada
-
-                Do
-                    Console.Write("Fecha de nacimiento: ")
-                    entrada = Console.ReadLine
-                    If Not Date.TryParse(entrada, mascota.FechaNacimiento) OrElse mascota.FechaNacimiento > Today Then
-                        Console.ForegroundColor = ConsoleColor.Red
-                        Console.WriteLine("Debes introducir una fecha anterior a la de hoy.")
-                        Console.ResetColor()
-                    End If
-                Loop While Not Date.TryParse(entrada, mascota.FechaNacimiento) OrElse mascota.FechaNacimiento > Today
-
-                If IsNothing(mascotaMasVieja) Then
-                    mascotaMasVieja = mascota
-                Else
-                    If mascota.FechaNacimiento < mascotaMasVieja.FechaNacimiento Then
-                        mascotaMasVieja = mascota
-                    End If
-                End If
-
+                ActualizarMascotaMasVieja(mascota)
                 persona.SuMascota = mascota
                 mascota.Dueño = persona
             End If
 
             Do
-                Console.Write("¿Otra persona? (S/N): ")
+                MostrarMensaje("¿Otra persona? (S/N): ", ConsoleColor.White)
                 entrada = Console.ReadLine.ToUpper
-                If entrada <> "S" AndAlso entrada <> "N" Then
-                    Console.ForegroundColor = ConsoleColor.Red
-                    Console.WriteLine("Introduce S o N.")
-                    Console.ResetColor()
+                If Not SioNo(entrada) Then
+                    MostrarMensaje("Debe introducir S / N", ConsoleColor.Red)
                 End If
-            Loop While entrada <> "S" AndAlso entrada <> "N"
+            Loop While SioNo(entrada) = False
 
 
-        Loop While entrada.Equals("S")
+        Loop While entrada = "S"
 
+        MostrarMensaje($"Datos de la persona más joven {vbCrLf}", ConsoleColor.Cyan)
+        MostrarMensaje($"Persona más joven: {_perMasJoven.Nombre} {vbCrLf} Fecha de nacimiento: {_perMasJoven.FechaNacimiento.ToLongDateString}", ConsoleColor.Blue)
 
-        Console.WriteLine("DATOS PERSONA MÁS JOVEN")
-        Console.ForegroundColor = ConsoleColor.Blue
-        Console.WriteLine()
-        Console.WriteLine("NOMBRE: " & perMasJoven.Nombre)
-        Console.WriteLine("FECHA DE NACIMIENTO: " & perMasJoven.FechaNacimiento)
-        If IsNothing(perMasJoven.SuMascota) Then
-            Console.WriteLine("NO TIENE MASCOTA")
+        If Not IsNothing(_mascotaMasVieja) Then
+            MostrarMensaje($"Datos de la mascota más vieja {vbCrLf}", ConsoleColor.Cyan)
+            MostrarMensaje($"Mascota más vieja: {_mascotaMasVieja.Nombre} {vbCrLf} Tipo: {_mascotaMasVieja.TipoMascota} {vbCrLf} Raza: {_mascotaMasVieja.Raza} {vbCrLf} Fecha de Nacimiento: {_mascotaMasVieja.FechaNacimiento.ToLongDateString} {vbCrLf} Dueño: {_mascotaMasVieja.Dueño.Nombre}", ConsoleColor.Blue)
         Else
-            Console.WriteLine("MASCOTA: " & perMasJoven.SuMascota.Nombre)
-        End If
-        Console.WriteLine()
-        Console.ResetColor()
-        Console.WriteLine("DATOS MASCOTA MÁS VIEJA")
-        If IsNothing(mascotaMasVieja) Then
-            Console.WriteLine("NADIE TIENE MASCOTAS")
-        Else
-            Console.ForegroundColor = ConsoleColor.Blue
-            Console.WriteLine()
-            Console.WriteLine("NOMBRE: " & mascotaMasVieja.Nombre)
-            Console.WriteLine("TIPO: " & mascotaMasVieja.TipoMascota)
-            Console.WriteLine("RAZA: " & mascotaMasVieja.Raza)
-            Console.WriteLine("FECHA NACIMIENTO: " & mascotaMasVieja.FechaNacimiento)
-            Console.WriteLine("DUEÑO: " & mascotaMasVieja.Dueño.Nombre)
-            Console.ResetColor()
+            MostrarMensaje("No hay mascotas", ConsoleColor.DarkGray)
         End If
     End Sub
-
+    Sub MostrarMensaje(mensaje As String, color As ConsoleColor)
+        Console.ForegroundColor = color
+        If color = ConsoleColor.White Then
+            Console.Write(mensaje)
+        Else
+            Console.WriteLine(mensaje)
+        End If
+        Console.ResetColor()
+    End Sub
+    Sub ActualizarPersonaMasJoven(per As Persona)
+        If IsNothing(_perMasJoven) OrElse per.FechaNacimiento > _perMasJoven.FechaNacimiento Then
+            _perMasJoven = per
+        End If
+    End Sub
+    Sub ActualizarMascotaMasVieja(masc As Mascota)
+        If IsNothing(_mascotaMasVieja) OrElse masc.FechaNacimiento < _mascotaMasVieja.FechaNacimiento Then
+            _mascotaMasVieja = masc
+        End If
+    End Sub
+    Function PedirTexto(mensaje As String) As String
+        Dim entrada As String
+        Do
+            MostrarMensaje(mensaje, ConsoleColor.White)
+            entrada = Console.ReadLine()
+        Loop While CaracteresValidos(entrada) = False
+        Return entrada
+    End Function
+    Function CaracteresValidos(palabra As String) As Boolean
+        Dim tieneNoLetras As Boolean
+        If String.IsNullOrWhiteSpace(palabra) Then
+            MostrarMensaje("El nombre no puede quedarse en blanco.", ConsoleColor.Red)
+            Return False
+        Else
+            For i = 0 To palabra.Length - 1
+                tieneNoLetras = Not (Char.IsLetter(palabra.ElementAt(i)) OrElse Char.IsWhiteSpace(palabra.ElementAt(i)))
+                If tieneNoLetras Then
+                    Exit For
+                End If
+            Next
+            If tieneNoLetras Then
+                MostrarMensaje("El nombre solo puede tener letras o espacios en blanco", ConsoleColor.Red)
+                Return False
+            Else
+                Return True
+            End If
+        End If
+    End Function
+    Function FechaNacimientoValida(fecha As String) As Boolean
+        Dim fechaAux As Date
+        If Not Date.TryParse(fecha, fechaAux) OrElse fechaAux > Today Then
+            MostrarMensaje("Debes introducir una fecha anterior a la de hoy.", ConsoleColor.Red)
+            Return False
+        Else
+            Return True
+        End If
+    End Function
+    Function SioNo(respuesta As String) As Boolean
+        Return respuesta = "S" OrElse respuesta = "N"
+    End Function
 End Module
